e0ecca1b23c8c9c1c3ea49aa52c01f3fcf8a158d
russellkoons <rkoons@gmail.com>
2020-09-29 07:10:26 -0400

    beers

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -7,6 +7,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/index.js")))
 }
 
