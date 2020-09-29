7c6b3d349f39fcc134dd5c061810189b1545d61b
russellkoons <rkoons@gmail.com>
2020-09-29 07:12:28 -0400

    pizzas

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---src-pages-404-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/404.js"))),
   "component---src-pages-beers-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/index.js"))),
-  "component---src-pages-orders-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/orders.js")))
+  "component---src-pages-orders-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/orders.js"))),
+  "component---src-pages-pizzas-js": hot(preferDefault(require("/home/russell/Desktop/projects/congratsby/gatsby/src/pages/pizzas.js")))
 }
 
---
+++ gatsby/src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzasPage() {
+  return (
+    <div>
+      <p>Getcha pizza here</p>
+    </div>
+  );
+}
