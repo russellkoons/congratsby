5e23ce8e847e1f5acba243d8e5528f11b6fa837c
russellkoons <rkoons@gmail.com>
2020-09-30 07:17:55 -0400

    fleshed out nav

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-function goToSlicemasters() {
-  console.log('Let us go ho')
-}
+// function goToSlicemasters() {
+//   // 1. Wait for 2 seconds
+//   setTimeout(() => {
+//     console.log('Let us go ho');
+//     // 2. Change page
+//     navigate('/slicemasters');
+//   }, 2000);
+// }
 
 export default function Nav() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Hot Now</Link>
         </li>
         <li>
-          <Link to="/beers">Beers</Link>
+          <Link to="/pizzas">Pizza Menu</Link>
         </li>
         <li>
+          <Link to="/">LOGO</Link>
+        </li>
+        <li>
+          <Link to="/slicemasters">Slicemasters</Link>
+        </li>
+        <li>
+          <Link to="/orders">Order Ahead</Link>
+        </li>
+        {/* <li>
           <button onClick={goToSlicemasters} type="button">
             Click me to see slicemasters after 2 seconds
           </button>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
