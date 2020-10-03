d37294d7116df17c03c7450c54cf2124ec669d5e
russellkoons <rkoons@gmail.com>
2020-10-03 13:39:31 -0400

    SiteBorderStyles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -6,6 +6,12 @@ import Nav from './Nav';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw ,12rem)
+`;
+
 const ContentStyles = styled.div`
   background: white;
   padding: 2rem;
@@ -13,14 +19,16 @@ const ContentStyles = styled.div`
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <ContentStyles>
-        <Nav />
-        {children}
-        <Footer />
-      </ContentStyles>
-    </div>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 }
