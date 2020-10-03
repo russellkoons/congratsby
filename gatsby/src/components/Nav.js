5872eea3c50ba205764b0f60de30ad562c5a47dc
russellkoons <rkoons@gmail.com>
2020-10-03 13:28:55 -0400

    a styling

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -29,6 +29,17 @@ const NavStyles = styled.nav`
     &:nth-child(4) {
       --rotate: 2.5deg;
     }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
   }
 `;
 
