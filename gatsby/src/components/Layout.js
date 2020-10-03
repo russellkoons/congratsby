6823ca0b5f4626c0bb4d5ddc26f64e85a794de7f
russellkoons <rkoons@gmail.com>
2020-10-03 13:36:45 -0400

    ContentStyles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import 'normalize.css';
 import Footer from './Footer';
 import Nav from './Nav';
-import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
-      <Footer />
+      <ContentStyles>
+        <Nav />
+        {children}
+        <Footer />
+      </ContentStyles>
     </div>
   );
 }
