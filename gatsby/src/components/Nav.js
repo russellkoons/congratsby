eb0a4fb87a7fa121ee23561902821fa287daa4ae
russellkoons <rkoons@gmail.com>
2020-10-03 13:50:04 -0400

    finesse

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -19,6 +19,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
 
   li {
@@ -43,6 +44,9 @@ const NavStyles = styled.nav`
     font-size: 3rem;
     text-decoration: none;
     &:hover {
+      color: var(--red);
+    }
+    &: [ aria-current = 'page' ] {
       color: var(--red);
     }
   }
