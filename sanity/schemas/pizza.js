63605b44a4871190ef426f309a9c99b0710be6d9
russellkoons <rkoons@gmail.com>
2020-10-04 06:35:19 -0400

    imported pizzas

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,5 +1,15 @@
 export default {
+  // Computer name
   name: 'pizza',
+  // Visible title
   title: 'pizzas',
   type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+  ],
 };
