2df5745fbc93c6797c3b6b9bdc70562ed82f0f47
russellkoons <rkoons@gmail.com>
2020-10-06 06:36:32 -0400

    toppings

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -50,7 +50,10 @@ export default {
     select: {
       title: 'name',
       media: 'image',
-      toppings: 'toppings',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
     },
     prepare: (fields) => {
       console.log(fields);
