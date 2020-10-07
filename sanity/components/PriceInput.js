e0c173dd5210c03f44c68bed95c4207f1e82ed91
russellkoons <rkoons@gmail.com>
2020-10-07 06:45:14 -0400

    filling out priceinput

--- sanity/components/PriceInput.js
+++ sanity/components/PriceInput.js
@@ -1,10 +1,10 @@
-import { func } from 'prop-types';
 import React from 'react';
 
-export default function PriceInput() {
+export default function PriceInput({ type, value, onChange, inputComponent }) {
   return (
     <div>
-      <h2>The Price</h2>
+      <h2>{type.title}</h2>
+      <p>{type.description}</p>
     </div>
   );
 }
