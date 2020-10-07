becfd351e6c15ce82ee8fdc78597d94b59211c37
russellkoons <rkoons@gmail.com>
2020-10-07 07:00:57 -0400

    formatMoney

--- sanity/components/PriceInput.js
+++ sanity/components/PriceInput.js
@@ -5,10 +5,17 @@ function createPatchFrom(value) {
   return PatchEvent.from(value === '' ? unset() : set(Number(value)));
 }
 
+const formatMoney = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+}).format;
+
 export default function PriceInput({ type, value, onChange, inputComponent }) {
   return (
     <div>
-      <h2>{type.title}</h2>
+      <h2>
+        {type.title} - {value ? formatMoney(value / 100) : ''}
+      </h2>
       <p>{type.description}</p>
       <input
         type={type.name}
