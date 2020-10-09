5e27d1f01e1ae7ed713510b19e115c415fb983fd
russellkoons <rkoons@gmail.com>
2020-10-09 06:34:13 -0400

    plugins

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,14 @@
 export default {
   siteMetadata: {
-    title: `Slicks Slices`,
+    title: `Slick's Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Atlanta`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-data',
+    },
+  ],
 };
