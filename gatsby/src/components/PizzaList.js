77a91a22f97b105faf60d73a40efb8da9d27dc24
russellkoons <rkoons@gmail.com>
2020-10-10 06:45:34 -0400

    SinglePizza

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+function SinglePizza({ pizza }) {
+  return <p>{pizza.name}</p>;
+}
+
 export default function PizzaList({ pizzas }) {
   return (
     <div>
       {pizzas.map((pizza) => (
-        <p>PIZZA</p>
+        <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
     </div>
   );
