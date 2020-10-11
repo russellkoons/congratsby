b42dce5cc8da79d5fa5649889d6d77bba3df8d6f
russellkoons <rkoons@gmail.com>
2020-10-11 07:10:47 -0400

    ToppingStyles

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import { graphql, Link, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
 
 function countPizzasInTopping(pizzas) {
   // Return the pizzas with counts
@@ -56,13 +80,13 @@ export default function ToppingsFilter() {
   // Loop over the first list of toppings and display the topping and the count of pizzas in that topping
   // Link it up!!
   return (
-    <div>
+    <ToppingsStyles>
       {toppingsWithCounts.map((topping) => (
         <Link to={`/topping/${topping.name}`} key={topping.id}>
           <span className="name">{topping.name}</span>
           <span className="count">{topping.count}</span>
         </Link>
       ))}
-    </div>
+    </ToppingsStyles>
   );
 }
