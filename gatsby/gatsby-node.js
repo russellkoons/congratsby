d035b81c0ec8accdbfbad16e1ca1de6300db8b0b
russellkoons <rkoons@gmail.com>
2020-10-13 05:56:11 -0400

    gatsby-node

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,13 @@
+function turnPizzasIntoPages(params) {
+  // Get a template for this page
+  
+  // Query all pizzas
+  // Loop over each and create a page for that pizza
+}
+
+export async function createPages(params) {
+  // Create pages dynamically
+  // 1. Pizzas
+  // 2. Toppings
+  // 3. Slicemasters
+}
