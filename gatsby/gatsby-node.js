d0a9898a523db3299cc7a1d79a37ba28d0b8baf1
russellkoons <rkoons@gmail.com>
2020-10-13 06:00:41 -0400

    turnPizzasIntoPages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,13 +1,17 @@
-function turnPizzasIntoPages(params) {
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
   // Get a template for this page
-  
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
   // Query all pizzas
+  const { data } = await graphql(``);
   // Loop over each and create a page for that pizza
 }
 
 export async function createPages(params) {
   // Create pages dynamically
   // 1. Pizzas
+  await turnPizzasIntoPages(params);
   // 2. Toppings
   // 3. Slicemasters
 }
