4feb1a3826d7829a9cd2c51f279ed2f75760b615
russellkoons <rkoons@gmail.com>
2020-10-16 07:08:49 -0400

    BeerGridStyles

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,5 +1,12 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
 
 export default function BeersPage({ data }) {
   return (
@@ -7,7 +14,7 @@ export default function BeersPage({ data }) {
       <h2 className="center">
         We have {data.beers.nodes.length} Beers Available. Dine in Only!
       </h2>
-      <div>
+      <BeerGridStyles>
         {data.beers.nodes.map((beer) => {
           const rating = Math.round(beer.rating.average);
           return (
@@ -15,16 +22,17 @@ export default function BeersPage({ data }) {
               <img src={beer.image} alt={beer.name} />
               <h3>{beer.name}</h3>
               {beer.price}
-              <p>
+              <p title={`${rating} out of 5 stars`}>
                 {`⭐`.repeat(rating)}
                 <span style={{ filter: `grayscale(100%)` }}>
                   {`⭐`.repeat(5 - rating)}
                 </span>
+                <span>({beer.rating.reviews})</span>
               </p>
             </div>
           );
         })}
-      </div>
+      </BeerGridStyles>
     </>
   );
 }
