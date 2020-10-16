ee6194920622a9b5f3643d3020b894cd2235ab79
russellkoons <rkoons@gmail.com>
2020-10-16 06:52:55 -0400

    beer query

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,28 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
+  
   return (
     <>
       <p>This is where the beer is</p>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
