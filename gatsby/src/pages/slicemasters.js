78b25e761c6e019ae04d45aecbbad2eaa62b0605
russellkoons <rkoons@gmail.com>
2020-10-17 11:42:37 -0400

    SlicemasterGrid

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,12 +1,19 @@
 import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
 
 export default function SlicemastersPage({ data }) {
   const slicemasters = data.slicemasters.nodes;
   return (
     <>
-      <div>
+      <SlicemasterGrid>
         {slicemasters.map((person) => (
           <div>
             <Link to={`slicemaster/${person.slug.current}`}>
@@ -18,7 +25,7 @@ export default function SlicemastersPage({ data }) {
             <p className="description">{person.description}</p>
           </div>
         ))}
-      </div>
+      </SlicemasterGrid>
     </>
   );
 }
