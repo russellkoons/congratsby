0d7c87ef06d8f6c2ef7e349da60648913562f591
russellkoons <rkoons@gmail.com>
2020-10-17 11:57:46 -0400

    SlicemasterStyles

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -9,13 +9,40 @@ const SlicemasterGrid = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
 `;
 
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
 export default function SlicemastersPage({ data }) {
   const slicemasters = data.slicemasters.nodes;
   return (
     <>
       <SlicemasterGrid>
         {slicemasters.map((person) => (
-          <div>
+          <SlicemasterStyles>
             <Link to={`slicemaster/${person.slug.current}`}>
               <h2>
                 <span className="mark">{person.name}</span>
@@ -23,7 +50,7 @@ export default function SlicemastersPage({ data }) {
             </Link>
             <Img fluid={person.image.asset.fluid} />
             <p className="description">{person.description}</p>
-          </div>
+          </SlicemasterStyles>
         ))}
       </SlicemasterGrid>
     </>
