7d2797601eee8f0edde89b2ca1ed3f4a7a263e80
russellkoons <rkoons@gmail.com>
2020-10-27 06:14:48 -0400

    SEO.js

---
+++ gatsby/src/components/SEO.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export default function SEO({ children, location, description, title, image }) {
+	<Helmet>
+		<html lang="en" />
+		
+	</Helmet>
+}
