e09f33269f92d0824a034a6a8de41a2f66ad864e
russellkoons <rkoons@gmail.com>
2020-10-28 05:33:34 -0400

    hook added

--- gatsby/src/pages/orders.js
+++ gatsby/src/pages/orders.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/SEO';
 
 export default function OrdersPage() {
+  const [name, setName] = useState('');
   return (
     <>
       <SEO title="Order a Pizza!" />
       <form>
         <fieldset>
           <legend>Your info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            htmlFor="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input type="email" htmlFor="email" />
         </fieldset>
         <fieldset>
           <legend>Menu</legend>
