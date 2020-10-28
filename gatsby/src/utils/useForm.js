9c66f744ff616bbfc3930a6dc5d690f695f3e219
russellkoons <rkoons@gmail.com>
2020-10-28 05:39:12 -0400

    useForm done

--- gatsby/src/utils/useForm.js
+++ gatsby/src/utils/useForm.js
@@ -4,11 +4,18 @@ export default function useForm(defaults) {
 	const [values, setValues] = useState(defaults);
 
 	function updateValue(e) {
+		// Check if it's a number and convert
+		let value = e.target.value;
+		if (e.target.type === 'number') {
+			value = parseInt(value);
+		}
 		setValues({
 			// copy existing values into it
 			...values,
 			// update the new values that changed
-			[e.target.name]: e.target.value,
+			[e.target.name]: value,
 		});
 	}
+
+	return { values, updateValue };
 }
