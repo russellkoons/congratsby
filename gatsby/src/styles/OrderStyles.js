5dad2230ac4d1b81644ee361b9a704a067a79106
russellkoons <rkoons@gmail.com>
2020-10-29 06:06:36 -0400

    began styling

--- gatsby/src/styles/OrderStyles.js
+++ gatsby/src/styles/OrderStyles.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
 const OrderStyles = styled.form`
-  
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  fieldset {
+    grid-column: span 2;
+    max-height: 600px;
+    overflow: auto;
+    display: grid;
+    gap: 1rem;
+    align-content: start;
+  }
 `;
 
 export default OrderStyles;
