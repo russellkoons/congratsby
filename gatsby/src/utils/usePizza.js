e4ca8a58bf354ab6654b7289e70e7fc94ba39b45
russellkoons <rkoons@gmail.com>
2020-10-30 05:29:51 -0400

    usePizza.js

---
+++ gatsby/src/utils/usePizza.js
@@ -0,0 +1,7 @@
+export default function usePizza({ pizzas, inputs }) {
+  // Create some state to hold our order
+  const
+  // Make a function to add things to order
+  // Make a function to remove things from order
+  // Send this data to a serverless function when they check out
+}
