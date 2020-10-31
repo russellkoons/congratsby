76a3c29dba05dd774369ddbdc8662684bd966868
russellkoons <rkoons@gmail.com>
2020-10-31 07:13:34 -0400

    calculateOrderTotal done

--- gatsby/src/utils/calculateOrderTotal.js
+++ gatsby/src/utils/calculateOrderTotal.js
@@ -1,8 +1,9 @@
+import calculatePizzaPrice from "./calculatePizzaPrice";
+
 export default function calculateOrderTotal(order, pizzas) {
-  // loop over each item in the order
-  const total = order.reduce((acc, singleOrder) => {
-    const pizza = pizzas.find(pizza => pizza.id === singleOrder.id);
+  const total = order.reduce((runningTotal, singleOrder) => {
+    const pizza = pizzas.find((singlePizza) => singlePizza.id === singleOrder.id);
+    return runningTotal + calculatePizzaPrice(pizza.price, singleOrder.size);
   }, 0);
-  // calculate the total for that pizza
-  // add that total to the running total
+  return total;
 }
