75f36a72ea2e56220afd5bd0c05b82be4bcabd69
russellkoons <rkoons@gmail.com>
2020-10-31 07:04:20 -0400

    calculateOrderTotal.js

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -9,6 +9,7 @@ import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
 import usePizza from '../utils/usePizza';
 import PizzaOrder from '../components/PizzaOrder';
+import calculateOrderTotal from '../utils/calculateOrderTotal';
 
 export default function OrderPage({ data }) {
   const pizzas = data.pizzas.nodes;
