816118b8fa70b45712409a0f9bfb3fba908d8878
russellkoons <rkoons@gmail.com>
2020-11-01 09:31:42 -0500

    OrderContext and OrderProvider

---
+++ gatsby/src/components/OrderContext.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+// create an order context
+const OrderContext = React.createContext();
+
+function OrderProvider({ children }) {
+  // we need to stick state in here
+}
