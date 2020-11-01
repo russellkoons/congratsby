07b8e74fb6492fcd15447ce820fdef62b0c92018
russellkoons <rkoons@gmail.com>
2020-11-01 09:34:29 -0500

    OrderProvider filled out

--- gatsby/src/components/OrderContext.js
+++ gatsby/src/components/OrderContext.js
@@ -1,8 +1,17 @@
+import { useStaticQuery } from 'gatsby';
 import React, { useState } from 'react';
 
 // create an order context
 const OrderContext = React.createContext();
 
-function OrderProvider({ children }) {
+export function OrderProvider({ children }) {
   // we need to stick state in here
+  const [order, setOrder] = useState('ass');
+  return (
+    <OrderContext.Provider>
+      {children}
+    </OrderContext.Provider>
+  );
 }
+
+export default OrderContext;
