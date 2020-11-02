1815df81ac7625823082759e7e10dcafcd5617e3
russellkoons <rkoons@gmail.com>
2020-11-02 06:34:39 -0500

    placeorder

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -2,10 +2,25 @@ const nodemailer = require('nodemailer');
 
 // create a transport
 const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
+  host: process.env.MAIL_HOST,
   port: 587,
   auth: {
-      user: 'derick.kutch@ethereal.email',
-      pass: 'YhcCT69mnrDJHy2ZUA'
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
   }
 });
+
+exports.handler = async (event, context) => {
+  // test send an email
+  const info = await transporter.sendMail({
+    from: "Slick's Slices <slick@example.com>",
+    to: "orders@example.com",
+    subject: "New order!",
+    html: `<p>Your new pizza order is here!</p>`,
+  });
+  console.log(info);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  }
+}
