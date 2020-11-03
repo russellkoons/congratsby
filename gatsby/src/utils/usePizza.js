daf5ce01c2da2ce05cbe015c6adc1ca8390a3c3d
russellkoons <rkoons@gmail.com>
2020-11-03 06:45:52 -0500

    fetching placeOrder

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import OrderContext from "../components/OrderContext";
+import attachNamesAndPrices from "./attachNamesAndPrices";
 import calculateOrderTotal from "./calculateOrderTotal";
 import formatMoney from "./formatMoney";
 
@@ -28,20 +29,40 @@ export default function usePizza({ pizzas, values }) {
     ]);
   }
 
-  // Send this data to a serverless function when they check out
   // Run when someone submits the forms
   async function submitOrder(e) {
     e.preventDefault();
     setLoading(true);
+    setError(null);
+    setMessage(null);
+    
     // gather the data
     const body = {
-      order,
+      order: attachNamesAndPrices(order, pizzas),
       total: formatMoney(calculateOrderTotal(order, pizzas)),
       name: values.name,
       email: values.email,
     }
-    console.log(body);
+    // Send this data to a serverless function when they check out
+    const res = await fetch(`${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const text = JSON.parse(await res.text());
+    // check if everything worked
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false); // turn off loading
+      setError(text.message);
+    } else {
+      // it worked!
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza!');
+    }
   }
+  
 
   return {
     order,
