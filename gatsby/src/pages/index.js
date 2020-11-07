7f120b91b44aeb39bbfc85aa25fcf28ca26f6e65
russellkoons <rkoons@gmail.com>
2020-11-07 11:53:09 -0500

    updating index.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <>
-      <p>Hey I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      
+    </div>
   );
 }
