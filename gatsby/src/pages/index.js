62e0f23192d9c8423b6b7c0e4f14544f9d027e74
russellkoons <rkoons@gmail.com>
2020-11-07 11:58:10 -0500

    CurrentlySlicing and HotSlices added

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  )
+}
+
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  )
+}
+
 export default function HomePage() {
   return (
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
-      
+      <div>
+        <CurrentlySlicing />
+        <HotSlices />
+      </div>
     </div>
   );
 }
