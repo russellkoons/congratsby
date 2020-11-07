3e843d2b6f8b0b3d0eaa77109745cc0812fb3932
russellkoons <rkoons@gmail.com>
2020-11-07 11:09:20 -0500

    building out sidebar

--- sanity/sidebar.js
+++ sanity/sidebar.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import S from '@sanity/desk-tool/structure-builder';
 
-
+// build a custom sidebar
+export default function Sidebar() {
+  return S.list().title(`Slick's Slices`).items([
+    // create a new subitem
+    S.listItem()
+      .title('Home Page')
+      .icon(() => <strong>🔥</strong>),
+  ])
+}
