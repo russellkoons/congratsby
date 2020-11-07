c312abebad7f11c1de92098366b37e1051f97d03
russellkoons <rkoons@gmail.com>
2020-11-07 12:06:54 -0500

    useState and useEffect added to useLatestData

--- gatsby/src/utils/useLatestData.js
+++ gatsby/src/utils/useLatestData.js
@@ -1,4 +1,13 @@
+import { useEffect, useState } from 'react';
+
 function useLatestData() {
   // hot slices
+  const [hotSlices, setHotSlices] = useState();
   // slicemasters
+  const [slicemasters, setSlicemasters] = useState();
+  // use a side effect to fetch the data from the graphql endpoint
+  useEffect(function() {
+    // when the component loads, fetch the data
+    
+  });
 }
