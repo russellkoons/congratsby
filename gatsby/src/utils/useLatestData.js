47b9e0408cd53b9c8c4c41d2556637d94c7b651b
russellkoons <rkoons@gmail.com>
2020-11-07 12:20:20 -0500

    added fetch and query to useEffect in useLatestData

--- gatsby/src/utils/useLatestData.js
+++ gatsby/src/utils/useLatestData.js
@@ -8,6 +8,26 @@ function useLatestData() {
   // use a side effect to fetch the data from the graphql endpoint
   useEffect(function() {
     // when the component loads, fetch the data
-    
-  });
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            StoreSettings(id: "downtown") {
+              name
+              slicemaster {
+                name
+              }
+              hotSlices {
+                name
+              }
+            }
+          }
+        `,
+      })
+    })
+  }, []);
 }
