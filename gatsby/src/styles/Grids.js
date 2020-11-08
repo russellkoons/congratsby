2711750306eb29f943347ff2bda9baab6fbfba17
russellkoons <rkoons@gmail.com>
2020-11-08 11:19:55 -0500

    ItemStyles styling

--- gatsby/src/styles/Grids.js
+++ gatsby/src/styles/Grids.js
@@ -14,5 +14,43 @@ export const ItemsGrid = styled.div`
 
 // single grid item (for home page)
 export const ItemStyles = styled.div`
-  background: red;
+  text-align: center;
+  position: relative;
+  img {
+    height: auto;
+    font-size: 0;
+  }
+  p {
+    top: 0;
+    transform: rotate(-2deg) translateY(-10px);
+    position: absolute;
+    width: 100%;
+    left: 0;
+    margin: 0;
+    font-size: 2rem;
+    font-size: clamp(12px, 5vw, 20px);
+  }
+  .mark {
+    display: inline;
+  }
+  @keyframes shine {
+    from {
+      background-position: 200%;
+    }
+    to {
+      background-position: -40px;
+    }
+  }
+  img.loading {
+    --shine: white;
+    --background: var(--grey);
+    background-image: linear-gradient(
+      90deg,
+      var(--background) 0px,
+      var(--shine) 40px,
+      var(--background) 80px
+    );
+    background-size: 500px;
+    animation: shine 1s infinite linear;
+  }
 `;
