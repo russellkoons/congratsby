d1f948c4e6c0b8410b403fef2db88a0ab3ba66b5
russellkoons <rkoons@gmail.com>
2020-11-08 11:22:15 -0500

    updated LoadingGrid

--- gatsby/src/components/LoadingGrid.js
+++ gatsby/src/components/LoadingGrid.js
@@ -6,7 +6,9 @@ export default function LoadingGrid({ count }) {
     <ItemsGrid>
       {Array.from({ length: count }, (_, i) => (
         <ItemStyles>
-          <p className="mark">Loading...</p>
+          <p>
+            <span className="mark">Loading...</span>
+          </p>
           <img
             src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAECAQAAADsOj3LAAAADklEQVR42mNkgANGQkwAAJoABWH6GPAAAAAASUVORK5CYII="
             className="Loading"
