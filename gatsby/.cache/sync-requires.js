230f531ae3d396af591adaa8d619280eaa80f414
russellkoons <rkoons@gmail.com>
2020-11-10 05:59:51 -0500

    gatsby build

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/404.js"))),
   "component---src-pages-beers-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/index.js"))),
---
+++ gatsby/public/component---src-pages-404-js-ad5bd073351647559cb9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return o}));var u=e("q1tI"),a=e.n(u);function o(){return a.a.createElement("p",null,"Hey, that page doesn't exist")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ad5bd073351647559cb9.js.map
