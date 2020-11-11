468bc381625a59e4af7ac2d7ab4bedc3bbcd3456
russellkoons <rkoons@gmail.com>
2020-11-11 06:35:12 -0500

    more nav responsiveness

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -51,11 +51,23 @@ const NavStyles = styled.nav`
       font-size: 2rem;
     }
   }
+
   @media (max-width: 600px) {
     --columns: 4;
+    margin-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+    padding-bottom: 2rem;
     ul {
       grid-template-rows: auto auto;
       grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+    .logo {
+      transform: none;
     }
   }
 `;
