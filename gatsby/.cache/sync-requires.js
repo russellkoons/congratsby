2885dd4f08d513ba6edf7b4a74f52c4f40362efe
russellkoons <rkoons@gmail.com>
2020-11-11 06:23:27 -0500

    keys on slicemasters page

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/404.js"))),
   "component---src-pages-beers-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": (preferDefault(require("/home/russell/projects/congratsby/gatsby/src/pages/index.js"))),
